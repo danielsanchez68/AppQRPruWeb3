d7e48ae90938a848859de49767cd0edab8e989a2
danielsanchez68 <danielsanchez68@hotmail.com>
2024-09-22 11:59:23 -0300

    v0.0.6.4.2

--- src/in/Adaptadores/http/UI.ts
+++ src/in/Adaptadores/http/UI.ts
@@ -1,5 +1,6 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import http from 'http';
+import path from 'path';
 
 
 import config from '../../../config'
@@ -16,14 +17,14 @@ import { IControladorUsuario } from './controladores/IUsuario';
 @injectable()
 class UI_HTTP implements IUI {
     constructor(
-        @inject(TYPES.IControladorMaq) private controladorMaq:IControladorMaq,
-        @inject(TYPES.IControladorUsuario) private controladorUsuario:IControladorUsuario
-    ) {}
+        @inject(TYPES.IControladorMaq) private controladorMaq: IControladorMaq,
+        @inject(TYPES.IControladorUsuario) private controladorUsuario: IControladorUsuario
+    ) { }
 
     private configRouterMaq() {
         const router = express.Router()
 
-        router.post('/consulta', this.controladorMaq.enviarConsultaMaquina )
+        router.post('/consulta', this.controladorMaq.enviarConsultaMaquina)
 
         return router
     }
@@ -31,28 +32,36 @@ class UI_HTTP implements IUI {
     private configRouterUsuario() {
         const router = express.Router()
 
-        router.get('/headers', this.controladorUsuario.headers )
+        router.get('/headers', this.controladorUsuario.headers)
 
         return router
     }
 
     public async start() {
         const app = express()
-
         app.use(cors())
-        app.use(express.static('public'))
         app.use(express.json())
-        
+
+        // --------------------------------------------------------------
+        //app.use(express.static('public'))
+        // Servir archivos estáticos desde la carpeta "public"
+        app.use(express.static(path.join(__dirname, 'public')));
+        // Ruta principal
+        app.get('/', (req: Request, res: Response) => {
+            res.sendFile(path.join(__dirname, 'public', 'index.html'));
+        });
+        // --------------------------------------------------------------
+
         // --------- Configuración de Rutas / endpoints ---------
         app.use('/api/maquina', this.configRouterMaq())
         app.use('/api/usuario', this.configRouterUsuario())
-        
+
         // --------------- Listen del Servidor ------------------
         const PORT = config.PORT
 
         const server = http.createServer(app).listen(PORT, () => console.log(`Server AppQR listen in http://localhost:${PORT}`))
         server.on('error', error => console.log(`Error en servidor: ${error.message}`))
-    }    
+    }
 }
 
 export default UI_HTTP
